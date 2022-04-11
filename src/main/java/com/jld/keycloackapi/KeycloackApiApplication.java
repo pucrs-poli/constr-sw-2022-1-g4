0ca4938c5f76be88b84b6f5a74b186b32a8c3606
DavidBNeto <netodavid25@hotmail.com>
2022-04-11 20:33:27 -0300

    project created

---
+++ src/main/java/com/jld/keycloackapi/KeycloackApiApplication.java
@@ -0,0 +1,13 @@
+package com.jld.keycloackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jld/keycloackapi/KeycloackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jld.keycloackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
